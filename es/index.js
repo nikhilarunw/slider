6176996b994f7d53fd14c9cf5ab824504958032c
Nikhil Wankhede <nikhil@kredx.com>
2017-07-17 09:22:07 +0530

    Added lib and es

---
+++ es/index.js
@@ -0,0 +1,10 @@
+import Slider from './Slider';
+import Range1 from './Range';
+import Handle1 from './Handle';
+import createSliderWithTooltip1 from './createSliderWithTooltip';
+
+export var Range = Range1;
+export var Handle = Handle1;
+export var createSliderWithTooltip = createSliderWithTooltip1;
+
+export default Slider;
